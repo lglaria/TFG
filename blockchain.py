b1b4c422659cee688306a4a5857272c473438d8e
lglaria <40244197+lglaria@users.noreply.github.com>
2018-12-17 19:35:18 +0100

    from py2 to py3

--- blockchain.py
+++ blockchain.py
@@ -12,18 +12,18 @@ import random
 
 #docstrings python, check
 class Blockchain(object): #pasar a mayuscula init
-    #formato bloques [hash_bloque_anterior, (data,data), timestamp, indice, nonce]
+	#formato bloques [hash_bloque_anterior, (data,data), timestamp, indice, nonce]
 	"""
 	formato bloques [hash_bloque_anterior, (data,data), timestamp, indice, nonce]
 	"""
 	def __init__(self):
 		self.cadena = []
 		self.longitud_nonce = 4 #DIFICULTAD DE POW se puede modificar
-        
+
 	def primer_bloque(self): #metodo para generar el primer bloque (cuando creamos la cadena)
 		primer_bloque = [0,0,int(round(time.time())),0]
 		self.cadena.append(self.pow(primer_bloque))
-        
+
 	def pow(self,bloque): #dado un bloque calcula un nonce
 		bloque_str = ''.join(str(e) for e in bloque)
 
@@ -37,48 +37,48 @@ class Blockchain(object): #pasar a mayuscula init
 	def calcular_bloque(self, dato1): #equivale al proceso de minado
 		ultimo_bloque = self.cadena[-1]
 		indice_anterior = ultimo_bloque[3]
-        #se calcula el hash del bloque anterior
-        hash_anterior = sha256(''.join(str(e) for e in ultimo_bloque)).hexdigest()
-        
-        timestamp = int(round(time.time()))
+		#se calcula el hash del bloque anterior
+		hash_anterior = sha256(''.join(str(e) for e in ultimo_bloque)).hexdigest()
 
-        bloque = self.pow([hash_anterior, dato1,timestamp, indice_anterior+1])
-      	self.cadena.append(bloque)                         
-        return bloque
-    
-    def validar_bloque(self, bloque, ultimo_bloque): #se comprueba si el bloque recibido es correcto respecto al ultimo bloque que tenemos
-        #se podria incluir alguna comprobacion adicional sobre el timestamp
-       # ultimo_bloque = self.cadena[-1] <- esta es la forma de obtener el ultimo bloque dada la cadena
-        hash_anterior = sha256(''.join(str(e) for e in ultimo_bloque)).hexdigest()
+		timestamp = int(round(time.time()))
 
-        if hash_anterior != bloque[0] or ultimo_bloque[3]+1 != bloque[3]: #se comprueba que coincidan tanto el hash anterior como que el indice sea consecutivo
+		bloque = self.pow([hash_anterior, dato1,timestamp, indice_anterior+1])
+		self.cadena.append(bloque)                         
+		return bloque
+
+	def validar_bloque(self, bloque, ultimo_bloque): #se comprueba si el bloque recibido es correcto respecto al ultimo bloque que tenemos
+	#se podria incluir alguna comprobacion adicional sobre el timestamp
+	# ultimo_bloque = self.cadena[-1] <- esta es la forma de obtener el ultimo bloque dada la cadena
+		hash_anterior = sha256(''.join(str(e) for e in ultimo_bloque)).hexdigest()
+
+		if hash_anterior != bloque[0] or ultimo_bloque[3]+1 != bloque[3]: #se comprueba que coincidan tanto el hash anterior como que el indice sea consecutivo
 			return False
-        if sha256(''.join(str(e) for e in bloque)).hexdigest()[:self.longitud_nonce] == '0'*self.longitud_nonce:
-            return True
-        else:
-            return False
+		if sha256(''.join(str(e) for e in bloque)).hexdigest()[:self.longitud_nonce] == '0'*self.longitud_nonce:
+			return True
+		else:
+			return False
 
-    def incluir_bloque(self,bloque):
-        if self.validar_bloque(bloque, self.cadena[-1]):
-            self.cadena.append(bloque)
+	def incluir_bloque(self,bloque):
+		if self.validar_bloque(bloque, self.cadena[-1]):
+			self.cadena.append(bloque)
 
-    def mostrar_datos(self, cadena):
+	def mostrar_datos(self, cadena):
 		datos = []
 		for bloque in cadena:
 			if bloque[3] > 0:
 				datos.append((bloque[1][0], bloque[1][2].x)) #devuelve el dato almacenado y la coordenada x de la clave publica de quien lo ha agregado
 		return datos
 
-    def validar_cadena(self, cadena):
-        for bloque in cadena:
-            if bloque[3] == 0: #si estamos con la cadena inicial
-                if sha256(''.join(str(e) for e in bloque)).hexdigest()[:self.longitud_nonce] != '0'*self.longitud_nonce:
-                    return False
-                bloque_anterior = bloque
-            else:
-                if not self.validar_bloque(bloque, bloque_anterior):
-                    return False
-                else:
-                    bloque_anterior = bloque
-        return True
-			
+	def validar_cadena(self, cadena):
+		for bloque in cadena:
+			if bloque[3] == 0: #si estamos con la cadena inicial
+				if sha256(''.join(str(e) for e in bloque)).hexdigest()[:self.longitud_nonce] != '0'*self.longitud_nonce:
+					return False
+				bloque_anterior = bloque
+			else:
+				if not self.validar_bloque(bloque, bloque_anterior):
+					return False
+				else:
+					bloque_anterior = bloque
+		return True
+
